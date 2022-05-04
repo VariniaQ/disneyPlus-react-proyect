889c4b9d30a5a45ecb5bef09b26e4371a4556982
VariniaQ <quisbertvarinia@gmail.com>
2022-05-04 16:54:36 -0300

    making a component to Tabs

--- src/pages/MovieDetailPage.js
+++ src/pages/MovieDetailPage.js
@@ -8,10 +8,12 @@ import Button from '@mui/material/Button';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import StarIcon from '@mui/icons-material/Star';
 
+
 // Components
 import Header from "../components/Header/Header"
 import SnackBarMessage from "../components/SnackBarMessage/SnackBarMessage";
 import SpinnerLoader from "../components/SpinnerLoader/SpinnerLoader"
+import BasicTabs from "../components/Tabs/Tabs";
 // CSS
 import './MovieDetail.css'
 // Services
@@ -59,6 +61,23 @@ const MovieDetailPage = () => {
                 }, 800))
     }, [])
 
+    const tabsContent = [
+        {
+            'title': "SUGGESTED",
+            'content': "SUG CONTENT"
+        },
+
+        {
+            'title': "EXTRAS",
+            'content': "EXTRAS CONTENT"
+        },
+
+        {
+            'title': "DETAILS",
+            'content': "DETAILS CONTENT"
+        },
+    ];
+
     return (
         <>
             {!token && <Navigate to="/login" />}
@@ -67,28 +86,29 @@ const MovieDetailPage = () => {
 
                 <CssBaseline />
                 <Container className="general-container" >
-                    {loader && <SpinnerLoader />}
-                    <img src={`https://image.tmdb.org/t/p/w500/${movieInfo.poster_path}`} alt={`{movieInfo.original_title}`} name={`{movieInfo.original_title}`}></img>
-                    <div className="container-movie-info">
-                        <h1>{movieInfo.original_title}</h1>
-                        <Button variant="contained" endIcon={<PlayArrowIcon />}>
-                            View now
-                        </Button>
-                        <h3>Description</h3>
-                        <p>{movieInfo.overview}</p>
-                        <h3><StarIcon fontSize={'small-x'} />Rating: <span>{movieInfo.vote_average} / 10</span></h3>
-                        <h3>Genres:</h3>
-                        <ul>
-                            {movieInfo.genres?.map((genre, key) => {
-                                return (
-                                    <li key={key}>
-                                        {genre.name}
-                                    </li>
-                                )
-                            })}
-                        </ul>
-                        {/*TODO add a Tab component */}
-                    </div>
+                    {loader ? <SpinnerLoader /> : (
+                        <>
+                            <img src={`https://image.tmdb.org/t/p/w500/${movieInfo.poster_path}`} alt={`{movieInfo.original_title}`} name={`{movieInfo.original_title}`}></img>
+                            <div className="container-movie-info">
+                                <h1>{movieInfo.original_title}</h1>
+                                <Button variant="contained" endIcon={<PlayArrowIcon />}>
+                                    View now
+                                </Button>
+                                <h3>Description</h3>
+                                <p>{movieInfo.overview}</p>
+                                <h3>Rating: <span>{movieInfo.vote_average} / 10</span></h3>
+                                <h3>Genres:</h3>
+                                <ul>
+                                    {movieInfo.genres?.map((genre, key) => {
+                                        return <li key={key}> {genre.name} </li>
+                                    })}
+                                </ul>
+
+                                <BasicTabs tabsContent={tabsContent} />
+                            </div>
+                        </>
+                    )}
+
                 </Container>
             </div>
             <SnackBarMessage
